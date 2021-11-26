c394a67632558b3e286d45ded51515ddace3ee67
Taso <taso@projecterror.dev>
2021-11-25 19:42:13 -0500

    fix(layout/header): don't pass non-existent props

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,19 +1,13 @@
-import { ArrowRightIcon } from '@heroicons/react/outline';
 import React, { useState } from 'react';
-import CountUp, { CountUpProps } from 'react-countup';
 import { MobileNavBar, DesktopNavBar } from './NavBar';
 import { GettingStartedDialog } from '../misc/GettingStartedDialog';
 
 const Header: React.FC = ({ children }) => {
-  const [zapDialogOpen, setZapDialogOpen] = useState(false);
   const [mobileNavOpen, setMobileNavOpen] = useState(false);
 
   return (
     <div>
-      <GettingStartedDialog
-        handleDialogClose={() => setZapDialogOpen(false)}
-        isDialogOpen={zapDialogOpen}
-      />
+      <GettingStartedDialog />
       <MobileNavBar isOpen={mobileNavOpen} onClose={() => setMobileNavOpen(false)} />
       <DesktopNavBar handleOpenMobile={() => setMobileNavOpen(true)} />
       <div className='flex h-screen items-center justify-center'>
