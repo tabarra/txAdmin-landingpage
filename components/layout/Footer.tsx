46dce861c6d7e41d4928da23a6ddfb81544da1b8
Noodles <jtqld@protonmail.com>
2022-08-19 20:49:04 +1000

    refactor: credit

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -31,7 +31,7 @@ export default function Footer() {
         </nav>
           <div className='mt-7 text-center text-base text-gray-400'>
         <p>txAdmin is licensed under the MIT license.</p>
-        <p>Page Design by Noodles#1234</p>
+        <p>Page Design by <a className='font-medium' href='https://github.com/goodnightnoodles'>Noodles</a>.</p>
         <p>&copy; 2019-2021 André Tabarra</p>
               <Link href="https://vercel.com/?utm_source=txadmin&utm_campaign=oss" passHref>
                   <a>
