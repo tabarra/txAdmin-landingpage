e22bad3df4e66963f541b03194ddcf9856e1643b
Noodles <jtqld@protonmail.com>
2022-09-07 20:57:52 +1000

    refactor: update contact info

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -31,7 +31,7 @@ export default function Footer() {
         </nav>
           <div className='mt-7 text-center text-base text-gray-400'>
         <p>txAdmin is licensed under the MIT license.</p>
-        <p>Page Design by <a className='font-medium' href='https://github.com/goodnightnoodles'>Noodles</a>.</p>
+        <p>Page Design by <a className='font-medium' href='https://joshblack.me'>Josh Black</a>.</p>
         <p>&copy; 2019-{new Date().getFullYear()} André Tabarra</p>
               <Link href="https://vercel.com/?utm_source=txadmin&utm_campaign=oss" passHref>
                   <a>
