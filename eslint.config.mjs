1cba58275f6f0dde1df24d3d1448df0718dc8af8
tabarra <1808295+tabarra@users.noreply.github.com>
2026-06-09 16:37:10 -0300

    chore: updated nodejs

---
+++ eslint.config.mjs
@@ -0,0 +1,7 @@
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals';
+
+const eslintConfig = [
+  ...nextCoreWebVitals,
+];
+
+export default eslintConfig;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,20 +1,14 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class txAdmin extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
-
   render() {
     return (
       <Html lang="en">
-        <Head>
           <body className="bg-neutral-900">
-            <Main />
-            <NextScript />
-          </body>
-        </Head>
+        <Head />
+          <Main />
+          <NextScript />
+        </body>
       </Html>
     )
   }
